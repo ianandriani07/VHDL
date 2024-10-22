17a51f8d66f62e14f3466b96dddd5dedff048f43
Fabioomega <fabioms2002@gmail.com>
2024-10-22 19:10:36 -0300

    Changed 8 inputs to vec

--- mux_input_8.vhd
+++ mux_input_8.vhd
@@ -31,8 +31,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity mux_input_8 is
     port (
-        in_1, in_2, in_3, in_4, in_5, in_6, in_7, in_8 : in std_logic;
-		  control : in std_logic_vector(2 downto 0);
+        in_vec : in std_logic_vector(7 downto 0);
+		control : in std_logic_vector(2 downto 0);
         mux8_out : out std_logic
     );
 end mux_input_8;
@@ -41,13 +41,13 @@ architecture Behavioral of mux_input_8 is
 begin
     with control (2 downto 0) select
     mux8_out <=
-        in_1 when "000",
-        in_2 when "001",
-        in_3 when "010",
-        in_4 when "011",
-        in_5 when "100",
-        in_6 when "101",
-        in_7 when "110",
-        in_8 when "111";
+        in_vec(0) when "000",
+        in_vec(1) when "001",
+        in_vec(2) when "010",
+        in_vec(3) when "011",
+        in_vec(4) when "100",
+        in_vec(5) when "101",
+        in_vec(6) when "110",
+        in_vec(7) when "111";
 end Behavioral;
 
